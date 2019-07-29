a6ddd5d99d260b0f72d8a4ae93ad2180d9d6e281
Amar singh <amarsengar2020@gmail.com>
2019-07-29 16:50:37 +0530

    .

---
+++ 1.py
@@ -0,0 +1,13 @@
+x = "hello"
+y = "hello"
+
+print(x is y)
+
+x = "hello!"
+y = "hello!"
+
+print(y is x)
+
+x = 's' * 20 is 'ssssssssssssssssssss'
+y = 's' * 21 is 'sssssssssssssssssssss'
+print(x,y)
